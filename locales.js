5ba9270cfb91f30554f46ccd1e1af81f8ffc932d
Sepent <work.sepent@gmail.com>
2023-04-22 16:05:32 +0700

    Create locales.js

---
+++ locales.js
@@ -0,0 +1,24 @@
+export default {
+  en: {
+    menu: {
+      loop: 'Loop this video',
+      copy_url: 'Copy video\'s url'
+    },
+
+    app: {
+      loading: 'Loading...',
+      not_support: 'Your browser does not support player.'
+    }
+  },
+  vi: {
+    menu: {
+      loop: 'Lặp video này',
+      copy_url: 'Copy đường dẫn video'
+    },
+
+    app: {
+      loading: 'Đang tải...',
+      not_support: 'Trình duyệt không hỗ trợ player'
+    }
+  }
+};
